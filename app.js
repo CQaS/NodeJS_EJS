c1b3d050eebbd01a2bd21e049e414e35ae18b9c5
Esteban Avila <cqasss@gmail.com>
2023-03-24 15:10:52 -0300

    dataTable_Templates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const router = require('./routes/router')
+const path = require('path')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({
@@ -8,6 +9,7 @@ app.use(express.urlencoded({
 }))
 
 app.use('/', router)
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.listen(3000, () => {
     console.log('SERVER on PORT:3000')
